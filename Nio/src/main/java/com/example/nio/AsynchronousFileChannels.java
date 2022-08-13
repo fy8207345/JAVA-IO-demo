aaf3a35436d5bce9e92994ff742bfd9cfbc94ba7
fengyan <fy@zwyun.cc>
2022-08-13 23:08:54 +0800

    asdas

--- Nio/src/main/java/com/example/nio/AsynchronousFileChannels.java
+++ Nio/src/main/java/com/example/nio/AsynchronousFileChannels.java
@@ -5,14 +5,47 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class AsynchronousFileChannels {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+//        read2();
+        readWithHandler();
+    }
+
+    public static void readWithHandler() throws URISyntaxException, IOException, InterruptedException {
+        URL resource = AsynchronousFileChannels.class.getResource("/a.txt");
+        Path path = Paths.get(resource.toURI());
+        AsynchronousFileChannel asynchronousFileChannel =
+                AsynchronousFileChannel.open(path, StandardOpenOption.READ);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        asynchronousFileChannel.read(buffer, 0, buffer, new CompletionHandler<Integer, ByteBuffer>() {
+            @Override
+            public void completed(Integer result, ByteBuffer attachment) {
+                System.out.println("result = " + result);
+
+                attachment.flip();
+                byte[] data = new byte[attachment.limit()];
+                attachment.get(data);
+                System.out.println(new String(data));
+                attachment.clear();
+            }
+
+            @Override
+            public void failed(Throwable exc, ByteBuffer attachment) {
+
+            }
+        });
+        TimeUnit.MILLISECONDS.sleep(500);
+    }
+
+    public static void read2() throws URISyntaxException, IOException {
         URL resource = AsynchronousFileChannels.class.getResource("/a.txt");
         Path path = Paths.get(resource.toURI());
         AsynchronousFileChannel asynchronousFileChannel =
